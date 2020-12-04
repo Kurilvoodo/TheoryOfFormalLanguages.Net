e8989ac0424e3f6251fc19efbcc351d4592fc871
Roman Vaskovtsov <roman.vas47@gmail.com>
2020-12-04 16:08:31 +0300

    Update the second task.

---
+++ Task2ForFormalLanguages/Task2ForFormalLanguages/Automation.cs
@@ -0,0 +1,153 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Task2ForFormalLanguages
+{
+    public class Automation
+    {
+        private string inputInformation;
+
+        private string startState;
+        private string finishState;
+
+        private string currentState;
+
+        private bool isRegexRecognize;
+
+        public int CountSkipSymbols { get; set; }
+
+        private IList<(string StateOut, IList<char> Value, string StateIn)> transitionMatrix;
+        private IList<(string StateOut, string RegexValue, string StateIn)> transitionMatrixRegex;
+
+        public Automation(string inputInformation, IList<(string StateOut, IList<char> Value, string StateIn)> transitionMatrix, string startState, string finishState)
+        {
+            this.inputInformation = inputInformation;
+            this.transitionMatrix = transitionMatrix;
+            this.startState = startState;
+            this.finishState = finishState;
+            this.currentState = this.startState;
+        }
+
+        public Automation(string inputInformation, JObject json)
+        {
+            if (!json["IsRegexRecognize"].Value<bool>())
+            {
+                IList<(string StateOut, IList<char> Value, string StateIn)> matrix = new List<(string StateOut, IList<char> Value, string StateIn)>();
+                foreach (var matrixRow in json["Matrix"].AsJEnumerable())
+                {
+                    string stateOut = matrixRow["StateOut"].Value<string>();
+                    string stateIn = matrixRow["StateIn"].Value<string>();
+                    IList<char> value = matrixRow["Value"].ToObject<List<char>>();
+                    matrix.Add((stateOut, value, stateIn));
+                }
+
+                this.transitionMatrix = matrix;
+            }
+            else
+            {
+                IList<(string StateOut, string RegexValue, string StateIn)> matrix = new List<(string StateOut, string RegexValue, string StateIn)>();
+                foreach (var matrixRow in json["Matrix"].AsJEnumerable())
+                {
+                    string stateOut = matrixRow["StateOut"].Value<string>();
+                    string stateIn = matrixRow["StateIn"].Value<string>();
+                    string value = matrixRow["Value"].Value<string>();
+                    matrix.Add((stateOut, value, stateIn));
+                }
+                this.transitionMatrixRegex = matrix;
+                this.isRegexRecognize = true;
+            }
+
+            this.inputInformation = inputInformation;
+            this.startState = json["StartState"].Value<string>();
+            this.finishState = json["FinishState"].Value<string>();
+            this.currentState = this.startState;
+
+        }
+
+        public (bool State, int CountRecognizeSymbols) Max()
+        {
+            currentState = startState;
+            if (isRegexRecognize)
+            {
+                int countRecognizeSymbols = 0;
+
+                while (true)
+                {
+                    if (RegexRecognizeSymbol(inputInformation, CountSkipSymbols))
+                    {
+                        countRecognizeSymbols++;
+                        CountSkipSymbols++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                if (countRecognizeSymbols > 0 && currentState == finishState)
+                {
+                    return (true, countRecognizeSymbols);
+                }
+                else
+                {
+                    return (false, countRecognizeSymbols);
+                }
+            }
+            else
+            {
+                int countRecognizeSymbols = 0;
+                while (currentState != finishState)
+                {
+                    if (RecognizeSymbol(inputInformation, CountSkipSymbols))
+                    {
+                        countRecognizeSymbols++;
+                        CountSkipSymbols++;
+                    }
+                    else
+                    {
+                        return (false, countRecognizeSymbols);
+                    }
+                }
+
+                return (true, countRecognizeSymbols);
+            }
+        }
+
+        private bool RecognizeSymbol(string inputInformation, int countSkipSymbols)
+        {
+            List<(string StateOut, IList<char> Value, string StateIn)> currentInformationForRecognize = transitionMatrix.ToList().FindAll(e => e.StateOut == currentState).ToList();
+
+            (string StateOut, IList<char> Value, string StateIn) state = currentInformationForRecognize.Find(e => e.Value.Contains(inputInformation[countSkipSymbols]));
+
+            if (state != default)
+            {
+                currentState = state.StateIn;
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool RegexRecognizeSymbol(string inputInformation, int countSkipSymbols)
+        {
+            if (countSkipSymbols == inputInformation.Length)
+            {
+                return false;
+            }
+
+            List<(string StateOut, string RegexValue, string StateIn)> currentInformationForRecognize = transitionMatrixRegex.ToList().FindAll(e => e.StateOut == currentState).ToList();
+
+            (string StateOut, string RegexValue, string StateIn) state = currentInformationForRecognize.Find(e => Regex.IsMatch(inputInformation[countSkipSymbols].ToString(), e.RegexValue));
+
+            if (state != default)
+            {
+                currentState = state.StateIn;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Task2ForFormalLanguages/Task2ForFormalLanguages/Program.cs
@@ -0,0 +1,81 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace Task2ForFormalLanguages
+{
+    class Program
+    {
+        static JObject ReadJson(string filename)
+        {
+            string json;
+            using (StreamReader reader = new StreamReader(filename))
+            {
+                json = reader.ReadToEnd();
+            }
+            return JObject.Parse(json);
+        }
+
+        static void Main(string[] args)
+        {
+            List<(Automation Automation, string Name)> automationAndNames = new List<(Automation, string)>();
+
+            string textForRecognize;
+
+            using (StreamReader reader = new StreamReader("code.txt"))
+            {
+                textForRecognize = reader.ReadToEnd();
+            }
+
+            automationAndNames.Add((new Automation(textForRecognize, ReadJson("intKeyword.json")), "IntKeyword"));
+            automationAndNames.Add((new Automation(textForRecognize, ReadJson("booleanKeyword.json")), "BooleanKeyword"));
+            automationAndNames.Add((new Automation(textForRecognize, ReadJson("doubleKeyword.json")), "DoubleKeyword"));
+            automationAndNames.Add((new Automation(textForRecognize, ReadJson("ifKeyword.json")), "IfKeyword"));
+            automationAndNames.Add((new Automation(textForRecognize, ReadJson("whileKeyword.json")), "WhileKeyword"));
+            automationAndNames.Add((new Automation(textForRecognize, ReadJson("falseKeyword.json")), "FalseKeyword"));
+            automationAndNames.Add((new Automation(textForRecognize, ReadJson("whitespaces.json")), "Whitespaces"));
+            automationAndNames.Add((new Automation(textForRecognize, ReadJson("arithmeticOperation.json")), "ArithmeticOperation"));
+            automationAndNames.Add((new Automation(textForRecognize, ReadJson("logicOperation.json")), "LogicOperation"));
+            automationAndNames.Add((new Automation(textForRecognize, ReadJson("floatWithEValue.json")), "FloatWithEValue"));
+            automationAndNames.Add((new Automation(textForRecognize, ReadJson("floatValue.json")), "FloatValue"));
+            automationAndNames.Add((new Automation(textForRecognize, ReadJson("intValue.json")), "IntValue"));
+            automationAndNames.Add((new Automation(textForRecognize, ReadJson("brackets.json")), "Brackets"));
+            automationAndNames.Add((new Automation(textForRecognize, ReadJson("comparisonOperator.json")), "ComparisonOperator"));
+            automationAndNames.Add((new Automation(textForRecognize, ReadJson("varNames.json")), "VarNames"));
+            automationAndNames.Add((new Automation(textForRecognize, ReadJson("serviceSymbol.json")), "ServiceSymbols"));
+
+            JArray resultJson = new JArray();
+
+            int countSkipSymbols = 0;
+            while (countSkipSymbols != textForRecognize.Length)
+            {
+                bool isRecognize = false;
+                foreach (var automation in automationAndNames)
+                {
+                    automation.Automation.CountSkipSymbols = countSkipSymbols;
+                    var result = automation.Automation.Max();
+                    if (result.State == true)
+                    {
+                        resultJson.Add(JToken.FromObject(new KeyValuePair<string, string>(automation.Name, textForRecognize.Substring(countSkipSymbols, result.CountRecognizeSymbols))));
+
+                        countSkipSymbols += result.CountRecognizeSymbols;
+                        isRecognize = true;
+                        break;
+                    }
+                }
+
+                if (!isRecognize)
+                {
+                    resultJson.Add(JToken.FromObject(new KeyValuePair<string, string>("Error recognize", countSkipSymbols.ToString())));
+                    break;
+                }
+            }
+
+            using (StreamWriter writer = new StreamWriter("result.json"))
+            {
+                writer.Write(resultJson.ToString());
+            }
+        }
+    }
+}
